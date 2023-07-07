05f6c7d71988ffc313c58854b6140fc6c987ed5c
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-07 15:39:43 +0330

    styling logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import css from "./Header.module.css";
-import Logo from '../../assets/logo.png'
+import Logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <div className={css.container}>
-          <div className={css.logo}><img src={Logo} alt="" />
-          <span>AMAZON HK</span></div>
+      <div className={css.logo}>
+        <img src={Logo} alt="" />
+        <span>AMAZON HK</span>
+      </div>
 
       <div className={css.right}>
-              <div className={css.menu}></div>
-              <input type="text" className={css.search} />
-              <span>PNG icon of card</span>
+        <div className={css.menu}>menu</div>
+        <input type="text" className={css.search} />
+        <span>PNG icon of card</span>
       </div>
     </div>
   );
