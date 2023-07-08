98ac8a5e0f21b59d003538c9bf24e6e0a2a89b92
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-09 02:14:07 +0330

    hero img bg fixing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import css from "./Hero.module.css";
 import sunskin from "../../assets/sun cream2/4_vinosun_cream_spf50_ingredients_caudalie_en.avif";
-import HeroImg from "../../assets/beauty/13990-[Converted] 5.png";
-import { RiShoppingBagFill } from "react-icons/ri"
-import { BsArrowRight } from "react-icons/bs"
+import HeroImg from "../../assets/sun screen cream/Honeyview_intro-1665702977-removebg-preview.png";
+import { RiShoppingBagFill } from "react-icons/ri";
+import { BsArrowRight } from "react-icons/bs";
 
 const Hero = () => {
   return (
@@ -12,28 +12,45 @@ const Hero = () => {
 
       <div className={css.h_sides}>
         <span>Skin Protection Cream</span>
+
         <div className="sunskin-img">
           <img src={sunskin} alt="" />
         </div>
 
         <div className={css.text2}>
           <span>Trendy Collection</span>
+
           <span>
+            {" "}
             Seedily say has suitable disposal and boy. Exercise joy man children
             rejoiced.
           </span>
         </div>
-        {/*middle side hero image  */}
-        <div className={css.wrapper}>
-          <div className={css.blueCircle}></div>
-          <img src={HeroImg} alt="" width={600} />
-          <div className={css.cart2}>
-            <RiShoppingBagFill />
-            <div className={css.signup}>
-              <span>Best signup Offers</span>
+      </div>
+
+      {/*middle side hero image  */}
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={HeroImg} alt="" width={600} />
+        <div className={css.cart2}>
+          <RiShoppingBagFill />
+          <div className={css.signup}>
+            <span>Best signup Offers</span>
+            <div>
+              <BsArrowRight />
             </div>
-            <BsArrowRight />
           </div>
+        </div>
+      </div>
+      {/* right side */}
+
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span>100K</span>
+          <span>Monthly Traffics</span>
+        </div>
+        <div className={css.customers}>
+          <span>Happy customers</span>
         </div>
       </div>
     </div>
