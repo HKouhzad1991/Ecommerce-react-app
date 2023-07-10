a279121cd740fdff39b5711cf5ee69254d7eb3fb
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-11 01:03:17 +0330

    swiper problem

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper";
 const Slider = () => {
   return (
     <div className="s-container">
-      <Swiper>
-        <SwiperSlide></SwiperSlide>
-      </Swiper>
     </div>
   );
 };
