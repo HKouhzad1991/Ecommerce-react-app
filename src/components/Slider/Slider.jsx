a39d740cc7b50630764b8f4a21201276813d25e6
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-13 01:26:04 +0330

    swiper fix problrm

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-// import Swiper and modules styles
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Pagination } from "swiper/modules";
+// // import Swiper and modules styles
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 
 import "./Slider.css";
 
