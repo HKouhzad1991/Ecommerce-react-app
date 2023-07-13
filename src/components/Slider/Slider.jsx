879b2d0a94676e989480867d9b49685f4dfa47c9
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-13 13:18:50 +0330

    fixing finally swiper slide with yarn

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 import "./Slider.css";
 
 const Slider = () => {
-  return <div className="s-container"></div>;
+  return <div className="s-container">
+    <Swiper>
+    <SwiperSlide>swiperslide</SwiperSlide>
+    </Swiper>
+  </div>;
 };
 
 export default Slider;
