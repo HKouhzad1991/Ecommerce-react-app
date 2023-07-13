93447b958e52c0b4ea793ec6d761fa6b14baf2f3
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-13 13:54:23 +0330

    slider settings

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,13 +3,17 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 //import swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 import "./Slider.css";
 
 const Slider = () => {
   return (
     <div className="s-container">
-      <Swiper>
+      <Swiper
+      slidesPerView={3}
+      >
         <SwiperSlide>1</SwiperSlide>
         <SwiperSlide>2</SwiperSlide>
         <SwiperSlide>3</SwiperSlide>
