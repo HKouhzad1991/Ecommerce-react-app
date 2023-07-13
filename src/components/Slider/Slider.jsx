fe19b9c493d605d2df4c147ad5f282cd17e40247
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-13 12:58:13 +0330

    fixed

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation, Pagination } from "swiper/modules";
-// // import Swiper and modules styles
-import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
 
 import "./Slider.css";
 
 const Slider = () => {
-  return (
-    <div className="s-container">
-      <Swiper>
-        <SwiperSlide1></SwiperSlide1>
-        <SwiperSlide2></SwiperSlide2>
-        <SwiperSlide3></SwiperSlide3>
-      </Swiper>
-    </div>
-  );
+  return <div className="s-container"></div>;
 };
 
 export default Slider;
