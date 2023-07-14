9281339eb34c4f5b7b2babc0f93afbdf71201576
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-14 14:48:05 +0330

    virtual spans

--- src/components/Virtual/Virtual.jsx
+++ src/components/Virtual/Virtual.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import css from "./Virtual.module.css";
 
 const Virtual = () => {
-  return <div className={css.Virtual}></div>;
+  return (
+    <div className={css.Virtual}>
+      <div className={css.left}>
+        <span>Virtual Try-On</span>
+        <span>Never Buy The Wrong Shade Again!</span>
+        <span>Try Now!</span>
+      </div>
+    </div>
+  );
 };
 
 export default Virtual;
