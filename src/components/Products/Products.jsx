396747ebc963a8ce614a7693e9e0948cf4f1d335
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-14 15:27:58 +0330

    importing product and menu

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import css from "./Products.module.css";
-import Plane from '../../assets/plane.png'
+import Plane from "../../assets/plane.png";
 
 const Products = () => {
   return (
     <div className={css.container}>
       <img src={Plane} alt="" />
+          <h1>Our Feature Products</h1>
+          <div className={css.Products}>
+          
+          </div>
     </div>
   );
 };
