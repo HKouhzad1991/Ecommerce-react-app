f046469fc027e8a8eff026a071777b0c7a037174
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-15 12:41:22 +0330

    use autoanimate

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import Plane from "../../assets/plane.png";
 import { ProductsData } from "../../data/products";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-
 const Products = () => {
+  const [parent] = useAutoAnimate();
   const [MenuProducts, setMenuProducts] = useState(ProductsData);
 
   const filter = (type) => {
@@ -18,7 +18,7 @@ const Products = () => {
       <h1>Our Feature Products 2023</h1>
       <div className={css.Products}>
         <ul className={css.menu}>
-          <li onClick={() =>setMenuProducts(ProductsData)}>All</li>
+          <li onClick={() => setMenuProducts(ProductsData)}>All</li>
           <li onClick={() => filter("skin care")}>Skin Care</li>
           <li onClick={() => filter("conditioner")}>Conditioners</li>
           <li onClick={() => filter("foundation")}>Foundation</li>
