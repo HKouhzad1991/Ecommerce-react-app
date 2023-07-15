0c61079c19ce57a179fd40381962ebcf28abb555
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-15 17:07:52 +0330

    creating footer divs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import css from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>footer</div>;
+    return <div className={css.cFooterWrapper}>
+        <hr />
+        
+        <div className={css.cFooter}>
+            <div className={css.cLogo}>
+            <img src={Logo} alt="" />
+            </div>
+        </div>
+    </div>;
 };
 
 export default Footer;
