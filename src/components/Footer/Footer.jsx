66e7acaba2e044092090130be75727e99e9a3bfb
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-16 00:39:44 +0330

    import phone icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,12 +21,24 @@ const Footer = () => {
           <img src={Logo} alt="" />
           <span>Hossein Kouhzad</span>
         </div>
+
         <div className={css.block}>
           <div className={css.detail}>
             <span>Contact Us</span>
-                      <span className={css.pngLine}>
-                      <LocationMarkerIcon className={css.icon} />
-                      </span>
+            <span className={css.pngLine}>
+              <LocationMarkerIcon className={css.icon} />
+              <span>11 north avenue Orlando, FL 32801</span>
+            </span>
+
+            <span className={css.pngLine}>
+              <PhoneIcon className={css.icon} />
+              <a
+                href="https://github.com/HKouhzad1991/HKouhzad1991"
+                className=""
+              >
+                support:Hossein-Kouhzad
+              </a>
+            </span>
           </div>
         </div>
       </div>
