850431bc4188c5c74b31fd4174c93c579fbbc471
HKouhzad1991 <https://github.com/HKouhzad1991/Ecommerce-react-app.git>
2023-07-15 12:57:21 +0330

    import testimo pic

--- src/components/Testimonials/Testimoinals.jsx
+++ src/components/Testimonials/Testimoinals.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import css from "./Testimonials.module.css";
+import Hero2 from "../../assets/testimonialHero.png";
 
 const Testimoinals = () => {
   return (
     <div className={css.testimnials}>
-          <div className={css.wrapper}>
-          
-          </div>
+      <div className={css.wrapper}>
+        <span>Top Rated</span>
+        <span>
+          SEEDILY SAY HAS SUITABLE DISPOSAL AND BOY. EXERCISE JOY MAN CHILDREN
+          REJOICED.
+        </span>
+      </div>
+      <img src={Hero2} alt="" />
     </div>
   );
 };
